00f21f54ec203d8935861b6df048b0122e54cba8
Onur Şentüre <woof.uyelik@gmail.com>
2017-04-24 19:56:11 +0300

    initial commit ⚡️

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+//SITE GLOBAL CONFIGURATION
+$email = "iletisim@seshouse.com"; 							//<-- Your email address
+$subscribe_email = "iletisim@seshouse.com"; 					//<<-- Email address for sending you subscribe notification
+$subscribe_notification_subject = "New site subscriber!"; 	//<<-- Email subscribe notification subject
+
+?>
